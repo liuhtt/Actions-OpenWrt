38d576d878a0f3c1a517201f2427f84c269a770f
liuhtt <70608598+liuhtt@users.noreply.github.com>
2026-06-06 14:29:41 +0800

    Update diy-part2.sh

--- Openwrt/diy-part2.sh
+++ Openwrt/diy-part2.sh
@@ -2,3 +2,4 @@
 #
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/CONFIG_TARGET_ROOTFS_PARTSIZE=.*/CONFIG_TARGET_ROOTFS_PARTSIZE=3096/g' .config
